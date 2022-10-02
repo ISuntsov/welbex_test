0fc947170d28530c319b5b795581e6942f4800e1
Ivan Suntsov <suncov-ivan@live.ru>
2022-10-02 16:04:27 +0300

    eslint bug3

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,39 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-    plugins: ['react'],
-    rules: {
-        semi: [1, 'always'],
-        indent: [0, 4],
-        'space-before-function-paren': [
-            'warn',
-            {anonymous: 'always', named: 'never'}
-        ],
-        quotes: [
-            'warn',
-            'single',
-            {
-                allowTemplateLiterals: true,
-                avoidEscape: true
-            }
-        ],
-        'no-trailing-spaces': [
-            'warn',
-            {
-                skipBlankLines: true,
-                ignoreComments: true
-            }
-        ],
-        'multiline-ternary': ['off']
-    }
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
 };
