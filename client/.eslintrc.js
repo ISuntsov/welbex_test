aec4326318c01c69bc2872b66aa71911d7003d62
Ivan Suntsov <suncov-ivan@live.ru>
2022-10-02 15:27:03 +0300

    eslint bug3

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ['plugin:react/recommended', 'standard'],
+    extends: ['plugin:react/recommended'],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
@@ -17,7 +17,7 @@ module.exports = {
         indent: [0, 4],
         'space-before-function-paren': [
             'warn',
-            { anonymous: 'always', named: 'never' }
+            {anonymous: 'always', named: 'never'}
         ],
         quotes: [
             'warn',
